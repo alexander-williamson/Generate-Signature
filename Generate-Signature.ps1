28f2de265fa5f6b57e20cde20c3553105e0ee95e
drogfild <35920772+drogfild@users.noreply.github.com>
2018-02-01 12:54:53 +0200

    Update Generate-Signature.ps1
    Reformat most of code. Deleted bunch of variables. Items you want to replace are defined in array after foreach statement (on line 19).

--- Generate-Signature.ps1
+++ Generate-Signature.ps1
@@ -1,60 +1,27 @@
 $strName = $env:username
+$strFilePrefix = "Company"
 
 # AD Filtering and fetching into $objUser
-$strFilter = "(&(objectCategory=User)(samAccountName=$strName))"
 $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
-$objSearcher.Filter = $strFilter
-$objPath = $objSearcher.FindOne()
-$objUser = $objPath.GetDirectoryEntry()
+$objSearcher.Filter = "(&(objectCategory=User)(samAccountName=$strName))"
+$objUser = $objSearcher.FindOne().GetDirectoryEntry()
 
+# Set signature path and file name
+$FolderLocation = [string]$Env:appdata + '\Microsoft\Signatures\'  
+# if path doesn't exist then create it
+if ( !$(Try { Test-Path $FolderLocation.trim() } Catch { $false }) ) { New-Item $FolderLocation -force }
+$fullOutputPath = $FolderLocation + $strFilePrefix + " " + [string]$objUser.name + ".htm"
 
-$strFullname = $objUser.FullName
-$arrFullnameParts = $strFullname.split(" ")
-$strFirstname = $arrFullnameParts[0]
-$strLastname = $arrFullnameParts[1]
-
-$strName = $objUser.FullName
-$strTitle = $objUser.Title
-# $strCompany = $objUser.Company
-# $strCred = $objUser.info
-# $strStreet = $objUser.StreetAddress
-$strMobile = $objUser.telephoneNumber
-# $strCity =  $objUser.l
-# $strPostCode = $objUser.PostalCode
-# $strCountry = $objUser.co
-$strEmail = if ($objUser.mail) { $objUser.mail.ToLower() }
-
-# $strWebsite = $objUser.wWWHomePage
-
-$strDepartment = "IT"
-
-$UserDataPath = $Env:appdata
-
-# todo: auto detect keys
-# if (test-path "HKCU:\\Software\\Microsoft\\Office\\11.0\\Common\\General") {
-#   get-item -path HKCU:\\Software\\Microsoft\\Office\\11.0\\Common\\General | new-Itemproperty -name Signatures -value signaturesCompany -propertytype string -force
-#   
-# 
-# if (test-path "HKCU:\\Software\\Microsoft\\Office\\12.0\\Common\\General") {
-#   get-item -path HKCU:\\Software\\Microsoft\\Office\\12.0\\Common\\General | new-Itemproperty -name Signatures -value signaturesCompany -propertytype string -force
-# }
-# if (test-path "HKCU:\\Software\\Microsoft\\Office\\14.0\\Common\\General") {
-#  get-item -path HKCU:\\Software\\Microsoft\\Office\\14.0\\Common\\General | new-Itemproperty -name Signatures -value signaturesCompany -propertytype string -force
-# }
-
-$FolderLocation = $UserDataPath + '\Microsoft\Signatures\'  
-mkdir $FolderLocation -force
-
-$fullOutputPath = $FolderLocation + $strFullname + ".htm"
+# read template file
+$strSignature = (Get-Content .\Template.html)
 
 # replace content in template
-# todo: refactor
-(Get-Content .\Template.html) | Foreach-Object {$_ -replace "%%FIRSTNAME%%", $strFirstname} | Foreach-Object {$_ -replace "%%LASTNAME%%", $strLastname} | Foreach-Object {$_ -replace "%%TITLE%%", $strTitle} | Foreach-Object {$_ -replace "%%DEPARTMENT%%", $strDepartment} | Foreach-Object {$_ -replace "%%MOBILE%%", $strMobile} | Foreach-Object {$_ -replace "%%EMAIL%%", $strEmail} | Set-Content $fullOutputPath
+foreach ($element in @('FullName','title','telephoneNumber','mail')) { 
+	$strSignature = $strSignature -replace "%%$element%%",[string]$objUser.$element 
+}
+
+# write new signature
+Set-Content -path $fullOutputPath $strSignature
 
 # output the path
-""
-"Successfully generated signature to: "
-$fullOutputPath
-""
-"Press any key to continue..."
-Read-Host
+"Successfully generated signature to: $fullOutputPath"
